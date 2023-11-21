4fad46cd5cea0fc1a79147866a61f11c650a40d9
zarnilinhtetDev <zarnilinhtet.mm@gmail.com>
2023-11-21 22:08:50 +0630

    latest

--- database/migrations/2023_11_21_145940_add_extra_col_to_users_table.php
+++ database/migrations/2023_11_21_145940_add_extra_col_to_users_table.php
@@ -12,11 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->string('employee_id')->nullable();
             $table->string('phone')->unique()->nullable();
             $table->string('nrc_number')->nullable();
             $table->date('birthday')->nullable();
             $table->enum('gender', ['male', 'female'])->nullable();
             $table->text('address')->nullable();
+            $table->bigInteger('department_id')->nullable();
+            $table->date('date_of_join')->nullable();
+            $table->boolean('is_present')->default(true);
         });
     }
 
@@ -26,7 +30,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn(['phone', 'nrc_number', 'birthday', 'gender', 'address', 'employee_id', 'department_id', 'date_of_join', 'is_present']);
         });
     }
 };
