7a1005fa103b9b361f6f5d0f8d0f35ac87f04f76
zarnilinhtetDev <zarnilinhtet.mm@gmail.com>
2023-11-29 13:28:26 -0600

    first

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -108,9 +108,54 @@
         </div> --}}
     </div>
     <div class="page-wrapper chiller-theme toggled">
-        <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
+        <a id="show-sidebar" class="btn btn-sm btn-dark mx-3" href="#">
             <i class="fas fa-bars"></i>
         </a>
+        <div class="">
+            <div class="header-menu">
+
+                <div class="row justify-content-center">
+                    <div class="">
+                        <div class="d-flex justify-content-center">
+
+                            @yield('title')
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="py-4">
+                @yield('content')
+            </div>
+            <div class="bottom-menu">
+
+                <div class="row  justify-content-center">
+                    <div class="col-md-8">
+                        <div class="d-flex justify-content-between">
+                            <a href="">
+                                <i class="fa-solid fa-house"></i>
+                                <p class="mb-0">Home</p>
+                            </a>
+                            <a href="">
+                                <i class="fa-solid fa-house"></i>
+                                <p class="mb-0">Home</p>
+                            </a>
+                            <a href="">
+                                <i class="fa-solid fa-house"></i>
+                                <p class="mb-0">Home</p>
+                            </a>
+
+
+                            <a href=""><i class="fa-solid fa-house"></i>
+                                <p class="mb-0">Home</p>
+                            </a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <nav id="sidebar" class="sidebar-wrapper">
             <div class="sidebar-content">
                 <div class="sidebar-brand">
@@ -310,92 +355,7 @@
         <!-- sidebar-wrapper  -->
         <main class="page-content">
             <div class="container-fluid">
-                <nav class="  navbar navbar-expand-md navbar-light bg-white shadow-sm">
-                    <div class="container">
 
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-                            <ul class="navbar-nav me-auto">
-
-                            </ul>
-
-                            <ul class="navbar-nav ms-auto">
-
-                                @guest
-                                    @if (Route::has('login'))
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                        </li>
-                                    @endif
-
-                                    @if (Route::has('register'))
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                        </li>
-                                    @endif
-                                @else
-                                    <li class="nav-item dropdown">
-                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#"
-                                            role="button" data-bs-toggle="dropdown" aria-haspopup="true"
-                                            aria-expanded="false" v-pre>
-                                            {{ Auth::user()->name }}
-                                        </a>
-
-                                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                                onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                                {{ __('Logout') }}
-                                            </a>
-
-                                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                                class="d-none">
-                                                @csrf
-                                            </form>
-                                        </div>
-                                    </li>
-                                @endguest
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-
-                <div class="py-4">
-                    @yield('content')
-                </div>
-                {{-- <div class="bottom-menu">
-
-                    <div class="row  justify-content-center">
-                        <div class="col-md-8">
-                            <div class="d-flex justify-content-between">
-                                <a href="">
-                                    <i class="fa-solid fa-house"></i>
-                                    <p class="mb-0">Home</p>
-                                </a>
-                                <a href="">
-                                    <i class="fa-solid fa-house"></i>
-                                    <p class="mb-0">Home</p>
-                                </a>
-                                <a href="">
-                                    <i class="fa-solid fa-house"></i>
-                                    <p class="mb-0">Home</p>
-                                </a>
-
-
-                                <a href=""><i class="fa-solid fa-house"></i>
-                                    <p class="mb-0">Home</p>
-                                </a>
-
-                            </div>
-                        </div>
-                    </div>
-                </div> --}}
             </div>
         </main>
         <!-- page-content" -->
